c282a133da5432e8743d333602806e6319087cc3
zakiroc <164227048+zakiroc@users.noreply.github.com>
2024-06-28 21:06:38 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,18 @@
+let obj = {
+
+}
+
+let prom = prompt('name')
+let prom_2 = prompt('spaceID')
+let prom_3 = prompt('space password')
+
+obj.name = prom
+obj.Id  = prom_2
+obj.password = prom_3
+console.log(obj);
+
+
+
+        
+        
+       
